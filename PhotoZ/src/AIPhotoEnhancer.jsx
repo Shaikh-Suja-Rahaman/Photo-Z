8b59bf8054c8381c72297732df81364eabf929cc
Shaikh-Suja-Rahaman <sujarahaman.dms.727@gmail.com>
2025-04-18 03:51:32 +0530

    start

---
+++ PhotoZ/src/AIPhotoEnhancer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from '../src/components/AIPhotoEnchancer/Home'
+
+const AIPhotoEnhancer =() =>{
+
+
+  return <div>
+
+    Header
+
+    <Home/>
+
+    footer
+
+    </div>
+
+}
+
+export default AIPhotoEnhancer
---
+++ PhotoZ/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import AIPhotoEnhancer from './AIPhotoEnhancer'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <AIPhotoEnhancer/>
+    </>
+  )
+}
+
+export default App
